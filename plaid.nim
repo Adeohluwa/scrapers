28a31103fc8b5d725244a0712f51ad161b064f08
Adeoluwa Adejumo <adejumoadeoluwa@gmail.com>
2020-01-24 13:34:22 +0100

    Plaid technical support role resume

---
+++ plaid.nim
@@ -0,0 +1,23 @@
+import os
+import httpclient
+import json
+import strutils
+
+var api = "https://contact.plaid.com/jobs"
+var client = newHttpClient()
+
+client.headers = newHttpHeaders({"Content-Type":"application/json"})
+
+while true:
+  let data = %*{
+    "name": "Adeoluwa Adejumo",
+    "email":"adejumoadeoluwa@gmail.com",
+    "resume":
+    "https://www.dropbox.com/s/p3o3hqqkfzuvpj0/Adeoluwa%20Adejumo%20%28technical%20support%29.pdf?dl=1",
+    "github": "https://github.com/adeohluwa",
+    "superpower": "b1 spanish"
+    }
+
+  echo "POSTING... ", $data
+  let response = client.post(api, $data)
+  echo "UPDATE: ", $response.body
